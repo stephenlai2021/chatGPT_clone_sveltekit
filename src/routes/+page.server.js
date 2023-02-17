532c5061f18afbf33d25119e768080428a80ef71
Stephen_Lai <stephenlai2021@gmail.com>
2023-02-17 22:58:26 +0800

    commit

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,7 +1,5 @@
 export const load = async ({ fetch }) => {
   const res = await fetch('/api/openai/generatePrompts')
   const json = await res.json()
-
-  // console.log('data: ', json.data)
   return { openai: json.data  }
 };
